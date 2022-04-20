78f02896d95840553c2a9e362383c26a55ba8cb9
Rohan Bulusu <rohanbulusu@Rohans-MacBook-Pro.local>
2022-04-20 16:40:32 -0400

    expenses are properly filtered by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,6 +14,10 @@ const Expenses = (props) => {
     // console.log(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
@@ -21,7 +25,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
